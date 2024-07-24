6c34c67895995bf903240b832c037f36b73edfd9
joey <786678257@qq.com>
2024-07-24 19:12:07 +0800

    init

---
+++ src/main/pages/home.tsx
@@ -0,0 +1,12 @@
+import Editor from '@/components/Editor'
+import React from 'react'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Editor />
+    </div>
+  )
+}
+
+export default Home
--- src/main/router.tsx
+++ src/main/router.tsx
@@ -1,10 +1,17 @@
 import { createHashRouter } from 'react-router-dom'
 import Layout from './layout'
+import Home from './pages/home'
 
 const router = createHashRouter([
   {
     path: '/',
     element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
   },
 ])
 
